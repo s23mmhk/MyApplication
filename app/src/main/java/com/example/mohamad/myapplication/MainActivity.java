32d6369db75d4b4b8df18468959805c6179d7440
s23mmhk <mohamadkassem2321@gmail.com>
2017-02-19 11:18:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mohamad/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.mohamad.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buClick(View view) {
+        Button b = (Button) findViewById(R.id.button);
+        b.setText("you are clicked me :)");
+    }
+}
